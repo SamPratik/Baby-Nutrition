e9c017bddab0da4e091c370bd9c0b00da7d5323c
SamPratik <pratik.anwar@gmail.com>
2018-02-28 19:22:14 +0600

    initial commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+
+<div style="height:45px; background:#000000;float:left; width:1350px; margin-top:20px;">
+	<p style="color:white;padding-top: 11px; float:left;margin-left: 480px;">Designed & developed by Samiul Alim Pratik &copy; 2016</p>
+</div>
